5c3f2664e735c476d021a616e139d0b977aa8c53
Michelle LaLonde <mlalonde309@gmail.com>
2020-09-29 17:33:21 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom'; 
+import './index.css'; 
+import App from './App'; 
+import STORE from './STORE';
+import participants from './participants';
+import chat from './chat';
+import stage from './stage';
 import './App.css';
+
+ReactDOM.render( <React.StrictMode> <App store = {STORE} /> </React.StrictMode>, document.getElementById('root') );
+
+import Split from './composition/Split';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className='App'>
+      <Split className='left' flexBasis={2}>
+        This is the content for the left `Split`. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+      </Split>
+      <Split className='right'>
+        This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
+      </Split>
+    </main>
+  )
 }
 
 export default App;
